430ef8a784f7f927ded0173191ff437480309f60
MdArifulislam21 <sabuj19977@gmail.com>
2021-07-08 21:27:05 +0600

    set debug=False

--- sells_report/settings.py
+++ sells_report/settings.py
@@ -17,10 +17,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qd0fltuu!2=p$-rx+g-c$=pk3cof6t7p9ui8v8r9mqk+c6o=2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "False"
 
 
-ALLOWED_HOSTS = ['seleschart.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -131,6 +131,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATIC_URL = '/static/'
+
+
 
 STATICFILES_DIRS = [
     BASE_DIR /'static',
